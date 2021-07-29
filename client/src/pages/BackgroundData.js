25c5d0f6bcc2ff1ac2885a2695e095d2015b45ed
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-07-29 14:42:01 +0300

    added default background images

---
+++ client/src/pages/BackgroundData.js
@@ -0,0 +1,102 @@
+export default [
+    {
+        img: 'https://scaleflex.airstore.io/demo/stephen-walker-unsplash.jpg',
+        title: 'img'
+    },
+    // {
+    //     img: 'http://localhost:3000/yas.png',
+    //     title: 'yas'
+    // },
+    // {
+    //     img: 'http://localhost:3000/img1.png',
+    //     title: 'img'
+    // },
+    // {
+    //     img: 'http://localhost:3000/img2.png',
+    //     title: 'img'
+    // },
+    // {
+    //     img: 'http://localhost:3000/img3.png',
+    //     title: 'img'
+    // },
+    {
+        img: 'http://localhost:3000/img4.jpg',
+        title: 'img'
+    },
+    {
+        img: 'http://localhost:3000/img5.jpeg',
+        title: 'img'
+    },
+    // {
+    //     img: 'http://localhost:3000/img5.jpeg',
+    //     title: 'img'
+    // },
+    {
+        img: 'http://localhost:3000/img6.jpeg',
+        title: 'img'
+    },
+    {
+        img: 'http://localhost:3000/img7.jpeg',
+        title: 'img'
+    },
+    {
+        img: 'http://localhost:3000/img8.jpeg',
+        title: 'img'
+    },
+    {
+        img: 'http://localhost:3000/img9.jpeg',
+        title: 'img'
+    },
+    {
+        img: 'http://localhost:3000/img10.jpeg',
+        title: 'img'
+    },
+    {
+        img: 'http://localhost:3000/img11.jpeg',
+        title: 'img'
+    },
+    {
+        img: 'http://localhost:3000/img12.jpeg',
+        title: 'img'
+    },
+    {
+        img: 'http://localhost:3000/img13.jpeg',
+        title: 'img'
+    },
+    {
+        img: 'http://localhost:3000/img14.jpeg',
+        title: 'img'
+    },
+    {
+        img: 'http://localhost:3000/img15.jpeg',
+        title: 'img'
+    },
+    {
+        img: 'http://localhost:3000/img16.jpeg',
+        title: 'img'
+    },
+    {
+        img: 'http://localhost:3000/img17.jpeg',
+        title: 'img'
+    },
+    {
+        img: 'http://localhost:3000/img18.jpeg',
+        title: 'img'
+    },
+    {
+        img: 'http://localhost:3000/img19.jpeg',
+        title: 'img'
+    },
+    {
+        img: 'http://localhost:3000/img20.jpeg',
+        title: 'img'
+    },
+    {
+        img: 'http://localhost:3000/img21.jpeg',
+        title: 'img'
+    },
+    {
+        img: 'http://localhost:3000/img22.jpeg',
+        title: 'img'
+    }
+];
--- client/src/pages/editor.js
+++ client/src/pages/editor.js
@@ -1,18 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FilerobotImageEditor from 'filerobot-image-editor';
-import { useHistory } from 'react-router-dom';
+import { makeStyles } from "@material-ui/core/styles";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import ListSubheader from "@material-ui/core/ListSubheader";
+
+import itemData from './BackgroundData';
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		display: "flex",
+		flexWrap: "wrap",
+		justifyContent: "space-around",
+		overflow: "hidden",
+		backgroundColor: theme.palette.background.paper,
+		padding: '3rem'
+	  },
+	  gridList: {
+		height: "auto"
+	  },
+	  icon: {
+		color: "rgba(255, 255, 255, 0.54)"
+	  },
+	  image: {
+		width: '500px',
+		"&:hover": {
+		  opacity: 0.25
+		}
+	  },
+	  header: {
+		  fontSize: 25,
+	  }
+}));
 
 export default function Editor() {
-    const src = 'https://scaleflex.airstore.io/demo/stephen-walker-unsplash.jpg';
-    const history = useHistory()
+    const classes = useStyles();
+	const [src, setSrc] = useState('');
 
   return (
     <div>
-      <FilerobotImageEditor
-        show={true}
-        src={src}
-        onClose={() => history.push('/account')}
-      />
+		<div className={classes.root}>
+        <GridList cellHeight={300} className={classes.gridList} cols={4}>
+          <GridListTile key="Subheader" cols={4} style={{ height: "auto"}}>
+            <ListSubheader className={classes.header} component="h3">Default Backgrounds</ListSubheader>
+          </GridListTile>
+          {itemData.map((tile, idx) => (
+            <GridListTile key={idx} onClick={() => setSrc(tile.img)}>
+              <img className={classes.image} src={tile.img} alt={tile.title} />
+            </GridListTile>
+          ))}
+        </GridList>
+      </div>
+    	<FilerobotImageEditor
+    		show={!!src}
+    		src={src}
+    		onClose={() => setSrc('')}
+    	/>
     </div>
   )
 }
