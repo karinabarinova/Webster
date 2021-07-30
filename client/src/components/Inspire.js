4cff664c9139f40717414d1a9ade4a562f656961
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-07-30 18:53:34 +0300

    Added Lightbox to inspiration page

--- client/src/components/Inspire.js
+++ client/src/components/Inspire.js
@@ -4,7 +4,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
 import ListSubheader from "@material-ui/core/ListSubheader";
-import itemData from '../helpers/BackgroundData';
+import Lightbox from 'react-image-lightbox';
 
 const useStyles = makeStyles((theme) => ({
 	root: {
@@ -32,6 +32,8 @@ const useStyles = makeStyles((theme) => ({
 export default function Inspire() {
     const classes = useStyles();
 	const [images, setImages] = useState([])
+	const [index, setIndex] = useState(0);
+	const [open, setOpen] = useState(false);
 
 	useEffect(() => {
 		axios.get(`https://picsum.photos/v2/list?page=${Math.floor(Math.random() * 25) + 1}&limit=20`)
@@ -50,11 +52,25 @@ export default function Inspire() {
                     <ListSubheader className={classes.header} component="h3">#GetInspired</ListSubheader>
                 </GridListTile>
                 {images.map((tile, idx) => (
-                    <GridListTile key={idx}>
+                    <GridListTile key={idx} onClick={() => {setIndex(idx); setOpen(true)}}>
                         <img className={classes.image} src={tile.download_url} alt={tile.author} />
                     </GridListTile>
                 ))}
             </GridList>
+			{open && (
+				<Lightbox
+				mainSrc={images[index].download_url}
+				nextSrc={images[(index + 1) % images.length].download_url}
+				prevSrc={images[(index - 1) % images.length].download_url}
+				onCloseRequest={() => setOpen(false)}
+				onMovePrevRequest={() =>
+				  setIndex((index - 1) >= 1 ? index - 1 : images.length - 1)
+				}
+				onMoveNextRequest={() =>
+					setIndex((index + 1) < images.length - 1 ? index + 1 : 1)
+				}
+			  />
+			)}
         </div>
     )
 }
