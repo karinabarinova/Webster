5e5a149e7bdfc44509e8173b50ed0410d61e9b55
codefactor-io <support@codefactor.io>
2021-08-04 15:52:25 +0000

    [CodeFactor] Apply fixes

--- sequelize/models/userprojects.js
+++ sequelize/models/userprojects.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
-  };
+  }
   UserProject.init({
     UserId: DataTypes.INTEGER,
     ProjectId: DataTypes.INTEGER
